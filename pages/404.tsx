096643316b9f7945ca4a871a6b44707fb923424a
soultree-fly <yuyaebean@gmail.com>
2021-02-23 23:55:23 +0900

    Feat: Create 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,67 @@
+import styled from "@emotion/styled";
+import Link from "next/link";
+
+const Self = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const ErrorMessageBlock = styled.div`
+  text-align: center;
+  padding: 0 20px;
+`;
+
+const ErrorCode = styled.h1`
+  font-size: 70px;
+  letter-spacing: -0.2px;
+  line-height: 100%;
+  margin: 0 0 11px;
+`;
+
+const PrimaryMessage = styled.p`
+  font-size: 21px;
+  letter-spacing: -0.3px;
+  margin: 0 0 5px;
+`;
+
+const SecondaryMessage = styled.p`
+  color: #999999;
+  font-size: 14px;
+  letter-spacing: -0.2px;
+  line-height: 150%;
+  margin: 0 0 5px;
+`;
+
+const GoToHomeButton = styled.a`
+  display: inline-block;
+  background: #fb8c00;
+  font-size: 15px;
+  font-weight: bold;
+  line-height: 24px;
+  padding: 11px 14px 12px;
+  margin: 33px auto 0;
+  cursor: pointer;
+`;
+
+function Custom404() {
+  return (
+    <Self>
+      <ErrorMessageBlock>
+        <ErrorCode>404</ErrorCode>
+        <PrimaryMessage>이 URL은 존재하지 않는 URL이에요</PrimaryMessage>
+        <SecondaryMessage>이런거 시도하지 마세요!</SecondaryMessage>
+        <Link href="/">
+          <GoToHomeButton>멋사 명지대 홈으로 가기</GoToHomeButton>
+        </Link>
+      </ErrorMessageBlock>
+    </Self>
+  );
+}
+
+export default Custom404;
