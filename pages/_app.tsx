2e5972a4905379588a7f76a66c252bf93177b7bc
soultree-fly <yuyaebean@gmail.com>
2021-02-24 21:22:25 +0900

    Feat: Add og and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import 'styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>멋쟁이 사자처럼 at 명지대 자연</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,94 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta property='og:title' content='멋쟁이 사자처럼 at 명지대 자연' />
+          <meta property='og:type' content='website' />
+          <meta property='og:url' content='https://mju-likelion.com/' />
+          <meta
+            property='og:description'
+            content='HACK YOUR LIFE! 멋쟁이 사자처럼 명지대 자연입니다.'
+          />
+          <meta property='og:locale' content='ko_KR' />
+          <meta property='og:locale:alternate' content='en_US' />
+
+          <link
+            rel='apple-touch-icon'
+            sizes='57x57'
+            href='/images/apple-icon-57x57.png'
+          />
+          <link
+            rel='apple-touch-icon'
+            sizes='60x60'
+            href='/images/apple-icon-60x60.png'
+          />
+          <link
+            rel='apple-touch-icon'
+            sizes='72x72'
+            href='/images/apple-icon-72x72.png'
+          />
+          <link
+            rel='apple-touch-icon'
+            sizes='76x76'
+            href='/images/apple-icon-76x76.png'
+          />
+          <link
+            rel='apple-touch-icon'
+            sizes='114x114'
+            href='/images/apple-icon-114x114.png'
+          />
+          <link
+            rel='apple-touch-icon'
+            sizes='120x120'
+            href='/images/apple-icon-120x120.png'
+          />
+          <link
+            rel='apple-touch-icon'
+            sizes='144x144'
+            href='/images/apple-icon-144x144.png'
+          />
+          <link
+            rel='apple-touch-icon'
+            sizes='152x152'
+            href='/images/apple-icon-152x152.png'
+          />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/images/apple-icon-180x180.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='192x192'
+            href='/images/android-icon-192x192.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/images/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='96x96'
+            href='/images/favicon-96x96.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/images/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/manifest.json' />
+          <meta name='msapplication-TileColor' content='#ffffff' />
+          <meta
+            name='msapplication-TileImage'
+            content='/images/ms-icon-144x144.png'
+          />
+          <meta name='theme-color' content='#ffffff' />
+        </Head>
         <body>
           <Main />
           <div id='modal' />
