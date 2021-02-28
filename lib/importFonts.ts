78c9a22f3edd807608801c35104a4ae1a5caca0f
soultree-fly <yuyaebean@gmail.com>
2021-02-28 11:09:26 +0900

    Feat: Change font to Noto Sans KR

--- lib/importFonts.ts
+++ lib/importFonts.ts
@@ -1,55 +1,12 @@
-const importSpoqaHanSans = `
-  @font-face {
-    font-family: 'Spoqa Han Sans Neo';
-    font-weight: 700;
-    src: local('Spoqa Han Sans Bold'),
-    url('/fonts/SpoqaHanSansNeo-Bold.woff2') format('woff2'),
-    url('/fonts/SpoqaHanSansNeo-Bold.woff') format('woff'),
-    url('/fonts/SpoqaHanSansNeo-Bold.ttf') format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Spoqa Han Sans Neo';
-    font-weight: 500;
-    src: local('Spoqa Han Sans Medium'),
-    url('/fonts/SpoqaHanSansNeo-Medium.woff2') format('woff2'),
-    url('/fonts/SpoqaHanSansNeo-Medium.woff') format('woff'),
-    url('/fonts/SpoqaHanSansNeo-Medium.ttf') format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Spoqa Han Sans Neo';
-    font-weight: 400;
-    src: local('Spoqa Han Sans Regular'),
-    url('/fonts/SpoqaHanSansNeo-Regular.woff2') format('woff2'),
-    url('/fonts/SpoqaHanSansNeo-Regular.woff') format('woff'),
-    url('/fonts/SpoqaHanSansNeo-Regular.ttf') format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Spoqa Han Sans Neo';
-    font-weight: 300;
-    src: local('Spoqa Han Sans Light'),
-    url('/fonts/SpoqaHanSansNeo-Light.woff2') format('woff2'),
-    url('/fonts/SpoqaHanSansNeo-Light.woff') format('woff'),
-    url('/fonts/SpoqaHanSansNeo-Light.ttf') format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Spoqa Han Sans Neo';
-    font-weight: 100;
-    src: local('Spoqa Han Sans Thin'),
-    url('/fonts/SpoqaHanSansNeo-Thin.woff2') format('woff2'),
-    url('/fonts/SpoqaHanSansNeo-Thin.woff') format('woff'),
-    url('/fonts/SpoqaHanSansNeo-Thin.ttf') format('truetype');
-  }
+const importNotoSans = `
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
 `;
 
 const importFonts = `
-  ${importSpoqaHanSans}
+  ${importNotoSans}
 
   html {
-    font-family: 'Spoqa Han Sans Neo', "Apple SD Gothic Neo", "Nanum Gothic", "Malgun Gothic", sans-serif;
+    font-family: 'Noto Sans KR', "Apple SD Gothic Neo", "Nanum Gothic", "Malgun Gothic", sans-serif;
   }
 `;
 
