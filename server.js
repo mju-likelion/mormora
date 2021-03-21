13ec8fb963a2197293c847554be8d4b11c36443e
ndaemy <yuyaebean@gmail.com>
2021-03-21 11:29:04 +0900

    Chore: Change local address

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.prepare().then(() => {
     handle(req, res, parsedUrl);
   }).listen(3000, err => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
+    console.log('> Ready on https://local.mju-likelion.com:3000');
   });
 });
